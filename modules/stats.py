70ebc258e89a3bb24e78771178719d4a0d7fa545
clerickbarrion <clerickbarrion@gmail.com>
2024-04-23 22:18:13 -0400

    added drug distrubtion by year function

--- modules/stats.py
+++ modules/stats.py
@@ -21,3 +21,44 @@ def deaths_by_year():
             if year in deaths_by_year:
                 deaths_by_year[year] += 1
     return deaths_by_year
+
+# gonna clean this up later
+def drug_distribution_by_year(year):
+    with open('data/Accidental_Drug_Related_Deaths_2012-2022.csv') as csvfile:
+        drugs = {
+            # Drug: [Deaths, Percentage]
+            'Alcohol': [0, 0],
+            'Heroin': [0, 0],
+            'Cocaine': [0, 0],
+            'Fentanyl': [0, 0],
+            'Oxycodone': [0, 0],
+            'Oxymorphone': [0, 0],
+            'Ethanol': [0, 0],
+            'Hydrocodone': [0, 0],
+            'Benzodiazepine': [0, 0],
+            'Methadone': [0, 0],
+            'Amphet': [0, 0],
+            'Tramad': [0, 0],
+            'Morphine': [0, 0],
+            'Xylazine': [0, 0],
+            'Gabapentin': [0, 0],
+            'Other': [0, 0],
+            'Total': 0
+        }
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if int(row['Date'][6:]) == year:
+                drug = row['Cause of Death']
+                keys = drugs.keys()
+                done = False
+                for key in keys:
+                    if key in drug:
+                        done = True
+                        drugs[key][0] += 1
+                        drugs['Total'] += 1
+                        drugs[key][1] = round(((drugs[key][0] / drugs['Total']) * 100),2)
+                    elif not done and key == 'Other':
+                        drugs['Other'][0] += 1
+                        drugs['Total'] += 1
+                        drugs['Other'][1] = round(((drugs['Other'][0] / drugs['Total']) * 100),2)
+    return drugs
--- project1_group4.py
+++ project1_group4.py
@@ -3,6 +3,7 @@ import modules.stats as stats
 
 # Uses the function stats.py inside the modules folder
 deaths_by_year = stats.deaths_by_year()
+drug_distribution_by_year = stats.drug_distribution_by_year(2014)
 
-print(deaths_by_year[2012])
-print(deaths_by_year[2013])
+print(deaths_by_year[2020])
+print(drug_distribution_by_year)
