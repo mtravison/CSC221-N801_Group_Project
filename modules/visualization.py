9677c77f1913f10df0d6d9d237a53da9cbb4b0ae
Giancarlo Kite <103792674+giancarlo-k@users.noreply.github.com>
2024-04-26 12:53:37 -0400

    Add files via upload

---
+++ modules/visualization.py
@@ -0,0 +1,24 @@
+# Giancarlo Kite will finish this once , work in progress
+
+import matplotlib.pyplot as plt
+
+def state_comparisons(states, start_date, end_date, chart_type):
+    pass
+
+def monthly_chart_multiple_states(states, year, chart_type):
+    pass
+
+def total_cases_percentage_deaths(year, chart_type):
+    pass
+
+def monthly_data_single_state(state, year, chart_type):
+    pass
+
+def monthly_data_multiple_states(states, year, chart_type):
+    pass
+
+
+
+
+
+
